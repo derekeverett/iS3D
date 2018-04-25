b739c0f796d31b7684e42e5f127c4fe66cee70f8
Michael <Michael@Michael-McNeliss-MacBook-Pro.local>
2018-04-25 00:34:43 -0400

    Got kaon spectra

--- src/cpp/emissionfunction.cpp
+++ src/cpp/emissionfunction.cpp
@@ -355,7 +355,7 @@ void EmissionFunctionArray::calculate_dN_ptdptdphidy(double *Mass, double *Sign,
             Yx = - uy / uperp;
             Yy = ux / uperp;
           }
-        } // set basis vectors
+        }
 
         // Set df coefficients: (efficient for openmp, etc?)
         // 14 moment
@@ -675,10 +675,10 @@ void EmissionFunctionArray::calculate_dN_ptdptdphidy(double *Mass, double *Sign,
                       // if feqmod breaks down, go back to case 2 statement (chapman enskog)
                       if(detA < 0.01)
                       {
-                        REGULATE_DELTAF = 1; 
+                        REGULATE_DELTAF = 1;
                         goto chapman_enskog;
                       }
-                      
+
                       // local momentum
                       double pX = -Xt * pt  +  Xx * px  +  Xy * py  +  Xn * tau2_pn;  // -x.p
                       double pY = Yx * px  +  Yy * py;                                // -y.p
