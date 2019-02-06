3e574a94eb1f0e85df1b22ffeee648914275f1ce
everett.165 <everett.165@unity-1.asc.ohio-state.edu>
2019-02-06 12:26:44 -0500

    this updates pdg storage arrays to hold a larger number of resonances and decay channels in the pdg table. also check to make sure mcid is everywhere a long int to avoid overflow

--- src/cpp/iS3D.h
+++ src/cpp/iS3D.h
@@ -11,8 +11,9 @@ const double two_pi = 2.0 * M_PI;
 const double two_pi2_hbarC3 = 2.0 * pow(M_PI, 2) * pow(hbarC, 3);
 const double four_pi2_hbarC3 = 4.0 * pow(M_PI, 2) * pow(hbarC, 3);
 
-const int Maxparticle = 400; //size of array for storage of the particles
-const int Maxdecaychannel = 17;
+//TO DO remove these hardcoded entries and declare a std::vector<particle_info> which can be appended
+const int Maxparticle = 600; //size of array for storage of the particles
+const int Maxdecaychannel = 50;
 const int Maxdecaypart = 5;
 
 class IS3D {
